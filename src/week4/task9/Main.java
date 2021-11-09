0ab596d70122710bd6a70cfe3292cc9515928a36
yeahmaybe <d.zarech@gmail.com>
2021-11-09 20:26:12 +0300

    Исправлено решение задачи 9

--- src/week4/task9/Main.java
+++ src/week4/task9/Main.java
@@ -7,22 +7,31 @@ import java.nio.file.Paths;
 public class Main {
 
     static void mergeLogs(String from) throws IOException {
-        String to = "logs.txt";
+
+        String to = "common.txt";
+        File common = new File(to);
+        if(common.createNewFile()) {} //создаем файл общего лога
 
         Object[] tree = Files.walk(Paths.get(from))
-                .filter(t -> t.toString().matches(".+\\.txt|.+\\.log|.+\\.trace")).toArray();
+                .filter(t -> t.toString().matches(".+\\.log|.+\\.trace")).toArray();
 
         for(Object logPath: tree){
-            String log = String.valueOf(logPath);
+
+
+
             File tmp = new File("tmp.txt");
-            if(tmp.createNewFile()) {};
+            if(tmp.createNewFile()) {} //создаем временный файл
+
+            String log = String.valueOf(logPath);
+            //будем объединять логи с файлом общего лога во временный файл,
+            //переименовывать его в файл общего лога, и так пока не закончатся логи
 
             try(BufferedReader fromOldLog = new BufferedReader(new FileReader(to));
                 BufferedReader fromNewLog = new BufferedReader(new FileReader(log));
                 BufferedWriter toTempLog = new BufferedWriter(new FileWriter(tmp))) {
 
-                String newLine = fromNewLog.readLine();
-                String oldLine = fromOldLog.readLine();
+                String newLine = fromNewLog.readLine(); //считываемый лог
+                String oldLine = fromOldLog.readLine(); //общий лог
 
                 while(newLine!=null && oldLine!=null) {
                     //время события из считываемого лога
@@ -40,7 +49,7 @@ public class Main {
                     }
                 }
 
-                //дописываем оставшиеся события
+                //дописываем оставшиеся события, если сортировать уже не надо
                 while(newLine!=null) {
                     toTempLog.write(newLine +'\n');
                     newLine = fromNewLog.readLine();
@@ -56,6 +65,7 @@ public class Main {
             }
         }
 
+        //выводим в консоль отсортированные события
         try(BufferedReader reader = new BufferedReader(new FileReader(to))) {
             String line = reader.readLine();
             while(line != null) {
@@ -63,6 +73,8 @@ public class Main {
                 line = reader.readLine();
             }
         }
+
+        if(new File(to).delete()) {}
     }
 
     public static void main(String[] args) throws IOException {
