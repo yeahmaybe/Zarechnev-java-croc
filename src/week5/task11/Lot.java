63d8382ff1f1bd0d0e1ff9344f23745fcb73bf03
yeahmaybe <d.zarech@gmail.com>
2021-11-16 20:39:01 +0300

    Исправлено решение задачи 11

--- src/week5/task11/Lot.java
+++ src/week5/task11/Lot.java
@@ -3,18 +3,28 @@ package week5.task11;
 import java.time.LocalDateTime;
 
 public class Lot {
-    double currentPrice = 0.0;
-    String leader = "Denis";
-    LocalDateTime finishTime = LocalDateTime.of(2021,11,11,11,11,11);
+    double currentPrice;
+    String leader;
+    LocalDateTime finishTime;
 
-    synchronized void updatePrice(double newPrice, String name) {
+    Lot(double startPrice, LocalDateTime finishTime) {
+        this.currentPrice = startPrice;
+        this.finishTime = finishTime;
+    }
+
+    void updatePrice(double newPrice, String name) {
+
         if(LocalDateTime.now().isBefore(finishTime) && newPrice > currentPrice) {
-            currentPrice = newPrice;
-            leader = name;
+            synchronized (this) {
+                if (LocalDateTime.now().isBefore(finishTime) && newPrice > currentPrice) {
+                    currentPrice = newPrice;
+                    leader = name;
+                }
+            }
         }
     }
 
-    synchronized String getLeader() {
+    String getLeader() {
         return leader;
     }
 }
